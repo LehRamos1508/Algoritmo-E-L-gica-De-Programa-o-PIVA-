eacf11880a91a2c4018ed7ceeb402d30aa6200d2
LehRamos1508 <125397754+LehRamos1508@users.noreply.github.com>
2023-06-13 14:55:05 -0300

    Delete Semana14 directory

--- Semana14/exercicio003.py
+++
@@ -1,9 +0,0 @@
-numeroString = input('Digite um número: ')
-soma, multiplicacao = 0, 1
-for i in numeroString:
-    numeroInt = int(i)
-    soma += numeroInt
-    multiplicacao *= numeroInt
-
-print(f'A soma de todos os dígitos do número {numeroString} é igual a {soma}!')
-print(f'A multiplicação de todos os dígitos do número {numeroString} é igual a {multiplicacao}!')
