9394f2ee9919d5d7be6c7380729ba1e70f4e58cb
LehRamos1508 <125397754+LehRamos1508@users.noreply.github.com>
2023-06-13 14:42:26 -0300

    Delete Semana15 directory

--- Semana15/exemplo001.py
+++
@@ -1,5 +0,0 @@
-arquivo = open('texto.txt', 'r', encoding='utf-8')
-texto = arquivo.read()
-print(texto)
-
-arquivo.close()
--- Semana15/exemplo002.py
+++
@@ -1,4 +0,0 @@
-texto = input('Digite um texto: ')
-arquivo = open('texto.txt', 'w', encoding='utf-8')
-arquivo.write(texto)
-arquivo.close()
